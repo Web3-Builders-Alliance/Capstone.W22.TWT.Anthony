6f7c6d06113ae766679d759e50fdab9ff6c17782
Canguilhem <clerger.a35@gmail.com>
2023-02-10 02:58:44 +0100

    use::cosmwasm-ts-codegen

---
+++ scripts/codegen.ts
@@ -0,0 +1,46 @@
+import codegen from "@cosmwasm/ts-codegen";
+
+codegen({
+  contracts: [
+    // {
+    //   name: "campaign",
+    //   dir: "./contracts/campaign/schema",
+    // },
+    // {
+    //   name: "factory",
+    //   dir: "./contracts/factory/schema",
+    // },
+    {
+      name: "receipt",
+      dir: "./contracts/campaign_receipt/schema",
+    },
+  ],
+  outPath: "./integration_tests/types/",
+
+  // options are completely optional ;)
+  options: {
+    bundle: {
+      bundleFile: "index.ts",
+      scope: "contracts",
+    },
+    types: {
+      enabled: true,
+    },
+    client: {
+      enabled: true,
+    },
+    reactQuery: {
+      enabled: false,
+    },
+    recoil: {
+      enabled: false,
+    },
+    messageComposer: {
+      enabled: false,
+    },
+  },
+})
+  .then(() => {
+    console.log("✨ all done!");
+  })
+  .catch(console.log);
